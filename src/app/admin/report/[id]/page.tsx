9ffa510ea5d8e8376ad69f3ea7ba052888b39ceb
gracekarinn <gracekarina12@gmail.com>
2025-05-02 11:01:29 +0700

    refactor: remove unused PageProps interface and simplify ReportDetailPage component

--- src/app/admin/report/[id]/page.tsx
+++ src/app/admin/report/[id]/page.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import AdminReportDetailSection from "@/modules/AdminReportModule/sections/AdminReportDetailSection";
 
-interface PageProps {
-    params: { id: string };
-}
-
-export default function ReportDetailPage({ params }: PageProps) {
-    console.log("Dari URL, reportId:", params.id);
-    return <AdminReportDetailSection reportId={params.id} />;
+export default function ReportDetailPage() {
+  return <AdminReportDetailSection />;
 }
